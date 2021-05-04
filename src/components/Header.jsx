a8eee70827269b7060c1d44c50de7f4f49787a63
sraone <sravanghanta27@gmail.com>
2021-05-04 20:43:35 +0530

    modification 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,42 @@ import Styled from 'styled-components'
 
 const Top = Styled.div`
     width:100%;
-    height:20%;
+    height:15vh;
     background-color: green;
     color:white;
+    display:flex;
+    justify-content: space-between;
+    align-items:center;
     padding:.5rem;
-    text-align : center;
-`
 
+    & h2{
+        padding-left:2rem;
+    }
+    
+`
+const Items = Styled.ul`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+
+    & li{
+        text-decoration:none;
+        color:white;
+        display:block;
+        padding:.2rem;
+        margin:.5rem;
+    }
+`;
 function Header() {
     return (
         <Top>
             <h2>header component</h2>
+            <Items>
+                <li>home</li>
+                <li>profile</li>
+                <li>services</li>
+                <li>contact us</li>
+            </Items>
         </Top>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Styled from 'styled-components'
 
+const Side = Styled.div`
+    width:25vw;
+    height:90vh;
+    align:left;
+    background-color:silver;
+`
 function Sidebar() {
     return (
-        <div>
+        <Side>
             <h2>side bar</h2>
-        </div>
+        </Side>
     )
 }
 
